bc77fd07f848cd629e688f38d094dd4da5c7e869
Ulan <ulantursunbekdev@gmail.com>
2021-08-03 01:32:10 +0600

    add styles to about and experience pages

--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -1,23 +1,46 @@
 import { useAppSelector } from "../store/hooks";
 import "../styles/aboutStyles.scss";
 import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
-
+import AvatarUlan from "../static/avatarUlan.png";
 const AboutComponent = () => {
   // const { route } = useAppSelector((state) => state.route);
   return (
     <div className="about">
       <div className="top-row">
-        <div>ULAN TURSUNBEK</div>
-        <div>
-          <button type="button">
-            <FaLinkedin />
-          </button>
-          <button type="button">
-            <FaGithubSquare />
-          </button>
+        <div className="top-row-title">ULAN TURSUNBEK</div>
+        <div className="links">
+          <a
+            href="https://www.linkedin.com/in/ulantursunbekuulu/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaLinkedin style={{ color: "white" }} />
+          </a>
+          <a
+            href="https://github.com/UlanTursunbek/ulan-site"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaGithubSquare style={{ color: "white" }} />
+          </a>
         </div>
       </div>
-      <div className="mid-row">mid row</div>
+      <div className="mid-row">
+        <div className="mid-row-left">
+          <img src={AvatarUlan} alt="avatar-ulan" />
+        </div>
+        <div className="mid-row-right">
+          <h3>Hello!</h3>
+          <p>
+            Greetings everyone. <br />
+            My name is Ulan, I'm front-end developer located in Bishkek,
+            Kyrgyzstan. <br />I have a serious passion in converting design and
+            ideas to UI and <br /> creating intuitive, dynamic user experience{" "}
+            <br /> Well-organizated person, problem solver, huge fan of FC
+            Arsenal, London. <br /> Interested in the entire frontend spectrum
+          </p>
+        </div>
+      </div>
       <div className="bot-row">bot row</div>
 
       {/* <h1>Hi! </h1>
--- src/components/ExperienceComponent.tsx
+++ src/components/ExperienceComponent.tsx
@@ -1,9 +1,72 @@
 import { useAppSelector } from "../store/hooks";
-import "../styles/infoComponent.scss";
+import "../styles/experienceStyles.scss";
+import ImgJobKovan from "../static/jobKovan.png";
+import ImgJobSkillCrucial from "../static/jobSkillCrucial.png";
+import ImgJobArgenta from "../static/jobArgenta.png";
 
+const experienceArray = [
+  { place: "freelace", url: null, title: "freelance", work: "design" },
+  {
+    place: "Kovan start up",
+    url: null,
+    title: "Kovan start up",
+    work: "solve UX problems",
+  },
+  {
+    place: "Kovan start up",
+    url: null,
+    title: "Kovan start up",
+    work: "solve UX problems",
+  },
+  {
+    place: "Kovan start up",
+    url: null,
+    title: "Kovan start up",
+    work: "solve UX problems",
+  }
+];
+const projectImages = [
+  {
+    src: ImgJobArgenta,
+    alt: "argenta",
+  },
+  {
+    src: ImgJobKovan,
+    alt: "argenta",
+  },
+  {
+    src: ImgJobSkillCrucial,
+    alt: "argenta",
+  },
+];
 const Experience = () => {
   const { route } = useAppSelector((state) => state.route);
-  return <div className="info">experience</div>;
+  return (
+    <div className="experience">
+      <div className="left-column">
+        {experienceArray.map((it) => {
+          return (
+            <div className="left-column-projects">
+              <div className="projects-title">{it.place}</div>
+              <div className="description">{it.work}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="right-column">
+        {projectImages.map((it) => {
+          return (
+            <div className="right-column-projects">
+              <div>
+              <img src={it.src} alt={it.alt} />
+
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
