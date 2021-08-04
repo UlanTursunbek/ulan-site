b6a58fed48528b2d46194f47c8a5e1c989c8a0fc
Ulan <ulantursunbekdev@gmail.com>
2021-08-04 13:22:41 +0600

    add experince

--- src/components/ExperienceComponent.tsx
+++ src/components/ExperienceComponent.tsx
@@ -3,27 +3,44 @@ import "../styles/experienceStyles.scss";
 import ImgJobKovan from "../static/jobKovan.png";
 import ImgJobSkillCrucial from "../static/jobSkillCrucial.png";
 import ImgJobArgenta from "../static/jobArgenta.png";
+import ImgJobRekom from "../static/jobRekom.png";
 
-const experienceArray = [
-  { place: "freelace", url: null, title: "freelance", work: "design" },
+interface IExperience {
+   title: string; work: string[];
+}
+
+const experienceArray: IExperience[] = [
   {
-    place: "Kovan start up",
-    url: null,
-    title: "Kovan start up",
-    work: "solve UX problems",
+    title: "Argenta",
+    work: [
+      " - visualize data from back end using amchart 4",
+      " - manage states of components with Redux-Saga and Mobx",
+      " - redesigned UI elements using SASS according to Figma"
+    ]
   },
   {
-    place: "Kovan start up",
-    url: null,
-    title: "Kovan start up",
-    work: "solve UX problems",
+    title: "Kovan studio",
+    work: [
+      " - Profiling, Troubleshooting & Bugs fixes that helped to tune system performance and decrease average response time",
+      " - Refactoring class components to hooks for better code",
+      " - Redesigned authorization pages",
+    ],
   },
   {
-    place: "Kovan start up",
-    url: null,
-    title: "Kovan start up",
-    work: "solve UX problems",
-  }
+    title: "SkillCrucial",
+    work: [
+      " - Cooperate with programmers to create clean responsive interfaces and simple, intuitive experiences",
+      " - Carried out unit tests to discover errors and optimize loading",
+      " - Improving product aesthetic and UX by using Tailwind.",
+    ],
+  },
+  {
+    title: "Freelance",
+    work: [
+      " - transfer design from Figma to web by using Bootstrap 4",
+      " - integrating with third-party services and external APIs",
+    ],
+  },
 ];
 const projectImages = [
   {
@@ -38,6 +55,10 @@ const projectImages = [
     src: ImgJobSkillCrucial,
     alt: "argenta",
   },
+  {
+    src: ImgJobRekom,
+    alt: "argenta",
+  },
 ];
 const Experience = () => {
   const { route } = useAppSelector((state) => state.route);
@@ -47,19 +68,23 @@ const Experience = () => {
         {experienceArray.map((it) => {
           return (
             <div className="left-column-projects">
-              <div className="projects-title">{it.place}</div>
-              <div className="description">{it.work}</div>
+              <div className="projects-title">{it.title}</div>
+              <ul className="description">
+                {it.work.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
             </div>
           );
         })}
       </div>
+      {/* http://www.zavod-rekom.kz/ */}
       <div className="right-column">
         {projectImages.map((it) => {
           return (
             <div className="right-column-projects">
               <div>
-              <img src={it.src} alt={it.alt} />
-
+                <img src={it.src} alt={it.alt} />
               </div>
             </div>
           );
